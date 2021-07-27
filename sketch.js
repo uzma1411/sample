d610b9b52e2280c38f4f49e1e881b879cd2f2bf0
Aarif <aarif@Aarifs-MacBook-Air.local>
2021-07-27 18:24:10 +0530

    added platform to d game

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,305,300,170)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -40,9 +41,7 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+   
     box1.display();
     box2.display();
     ground.display();
@@ -59,4 +58,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display()
 }
